c72caba49a4be484df73a0ea59677f2cb992c36b
jacksonwang7281 <jacksonwang7281@livemail.tw>
2023-11-10 14:13:07 +0800

    add a message 2 in local branch

--- pwm_test.c
+++ pwm_test.c
@@ -30,6 +30,7 @@ extern void pwm_minus_duty(int* fd,int* pwm_duty);
 
 //add a message to remote branch 1 in local
 //add a message to remote branch 1 in github
+//add a message to remote branch 2 in local
 
 
 int main(int argc, char *argv[])
